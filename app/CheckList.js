1029d32d03ac666f349b55013fd9b93b20bb3baf
darwin <oleg.umarov@gmail.com>
2016-02-17 20:48:47 -0600

    added checklist file

---
+++ app/CheckList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class CheckList extends Component {
+  render() {
+    let tasks = this.props.tasks.map(task => {
+      return  <li className="checklist_task">
+        <input type="checkbox" defaultChecked={ task.done } />
+        { task.name }
+        <a href='#' className="checklist_task--remove" />
+      </li>
+  });
+
+  return (
+    <div >
+      <ul className="checklist"> { tasks } </ul>
+    </div>
+  );
+}
+}
