adca63640279d5ee351f2e66deb5333abdeb0a6d
darwin <oleg.umarov@gmail.com>
2016-02-17 20:47:57 -0600

    added style

--- app/App.js
+++ app/App.js
@@ -1,1 +1,39 @@
-import React, { Component } from 'react';import {render} from 'react-dom';class App extends Component {  render(){    return (      <h1>Hello World</h1>    );  }}render(<App />, document.getElementById('root'));
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import KanbanBoard from './KanbanBoard';
+
+let cardList = [
+  {
+    id: 1,
+    title: 'Read the Book',
+    description: 'Read the whole book',
+    status: 'in-progress',
+    tasks: []
+  },
+  {
+    id: 2,
+    title: 'Write some code',
+    description: 'Code along with samples in the book',
+    status: 'todo',
+    tasks: [
+      {
+        id: 1,
+        name: 'Kanban Example',
+        done: false
+      },
+      {
+        id: 2,
+        name: 'Conctact List People',
+        done: false
+      },
+      {
+        id: 3,
+        name: 'Eventify',
+        done: true
+      }
+    ]
+  }
+];
+
+
+render(<KanbanBoard cards={cardList} />, document.getElementById('root'));
